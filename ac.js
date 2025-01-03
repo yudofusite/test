df9bd0cb99c84b1eb6056e0f28d6c48020a80b57
yudofusite <yasutaka82@icloud.com>
2025-01-03 19:42:49 +0900

    Update ac.js

--- ac.js
+++ ac.js
@@ -3,3 +3,12 @@ let editor = document.getElementById("editor");
 editor.setAttribute("width","600px");
 editor.setAttribute("height","400px");
 let ca0 = editor.getContext("2d");
+
+let mousex = 0;
+let mousey = 0;
+editor.onmousemove = function(mevent) {
+mousex = mevent.offsetX;
+mousey = mevent.offsetY;
+ca0.clearRect(0, 0, 600, 400);
+ca0.fillRect(mousex, mousey, 4, 4);
+};
