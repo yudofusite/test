17e3d8d383132f64ea943e08a36ad7cea6972aed
yudofusite <yasutaka82@icloud.com>
2025-01-04 19:25:57 +0900

    Update ac.js

--- ac.js
+++ ac.js
@@ -4,7 +4,6 @@ editor.setAttribute("width","600px");
 editor.setAttribute("height","400px");
 let ca0 = editor.getContext("2d");
 
-console.log(editor.clientWidth);
 
 let mousex = 0;
 let mousey = 0;
@@ -13,12 +12,11 @@ mousex = mevent.offsetX;
 mousex = mousex * (600 / editor.clientWidth);
 mousey = mevent.offsetY;
 mousey = mousey * (400 / editor.clientHeight);
-ca0.clearRect(0, 0, 600, 400);
-ca0.fillRect(mousex, mousey, 4, 4);
 };
 
 let md = 0;    //マウス押下（1）
 
+//--------------
 addEventListener("mousedown",function() {
   md = 1;
   console.log(md)
@@ -27,3 +25,10 @@ addEventListener("mouseup",function() {
   md = 0;
   console.log(md)
 })
+//--------------
+
+//メインループ
+let mainLoop = setInterval(function() {
+  ca0.clearRect(0, 0, 600, 400);
+  ca0.fillRect(mousex, mousey, 4, 4);
+},1000 / 60);
